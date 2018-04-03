9de6669499ad735a74c8c99751c8e550f887b847
Mario HD <mariohdpz@gmail.com>
2018-04-03 12:21:15 -0500

    Lang spanish ready

---
+++ index.php
@@ -0,0 +1,422 @@
+<!DOCTYPE html>
+<html dir="ltr" lang="en-US">
+<?php include('languages/es.php'); ?>
+<head>
+
+	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+	<meta name="author" content="Mario HD" />
+
+	<!-- Stylesheets
+	============================================= -->
+	<link href="https://fonts.googleapis.com/css?family=Lato:300,400,400i,700|Raleway:300,400,500,600,700|Crete+Round:400i" rel="stylesheet" type="text/css" />
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+	<link rel="stylesheet" href="style.css" type="text/css" />
+	<link rel="stylesheet" href="css/swiper.css" type="text/css" />
+	<link rel="stylesheet" href="css/dark.css" type="text/css" />
+	<link rel="stylesheet" href="css/font-icons.css" type="text/css" />
+	<link rel="stylesheet" href="css/animate.css" type="text/css" />
+	<link rel="stylesheet" href="css/magnific-popup.css" type="text/css" />
+
+	<link rel="stylesheet" href="css/responsive.css" type="text/css" />
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+
+	<!-- Document Title
+	============================================= -->
+	<title><?php echo $lang['main']['company']; ?></title>
+
+</head>
+
+<body class="stretched">
+
+	<!-- Document Wrapper
+	============================================= -->
+	<div id="wrapper" class="clearfix">
+
+		<div id="home" class="page-section" style="position:absolute;top:0;left:0;width:100%;height:200px;z-index:-2;"></div>
+
+		<section id="slider" class="slider-element slider-parallax full-screen with-header swiper_wrapper clearfix">
+
+			<div class="slider-parallax-inner">
+
+				<div class="swiper-container swiper-parent">
+					<div class="swiper-wrapper">
+						<div class="swiper-slide dark" style="background-image: url('images/oil/port-arthur.jpg');">
+							<div class="container clearfix">
+								<div class="slider-caption slider-caption-center">
+									<h2 data-caption-animate="fadeInUp"><?php echo $lang['main']['company']; ?></h2>
+									<p class="d-none d-sm-block" data-caption-animate="fadeInUp" data-caption-delay="200"><?php echo $lang['main']['description']; ?></p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+
+			</div>
+
+		</section>
+
+		<!-- Header
+		============================================= -->
+		<header id="header" class="full-header">
+
+			<div id="header-wrap">
+
+				<div class="container clearfix">
+
+					<div id="primary-menu-trigger"><i class="icon-reorder"></i></div>
+
+					<!-- Logo
+					============================================= -->
+					<div id="logo">
+						<a href="index.html" class="standard-logo" data-dark-logo="images/logo-dark.png"><img src="images/tasc-logo.png" alt="<?php echo $lang['main']['company']; ?>"></a>
+						<a href="index.html" class="retina-logo" data-dark-logo="images/logo-dark@2x.png"><img src="images/tasc-logo.png" alt="<?php echo $lang['main']['company']; ?>"></a>
+					</div><!-- #logo end -->
+
+					<!-- Primary Navigation
+					============================================= -->
+					<nav id="primary-menu">
+
+						<ul class="one-page-menu" data-easing="easeInOutExpo" data-speed="1500">
+							<li><a href="#" data-href="#section-about"><div><?php echo $lang['menu']['about']; ?></div></a></li>
+							<li><a href="#" data-href="#section-work"><div><?php echo $lang['menu']['work']; ?></div></a></li>
+							<li><a href="#" data-href="#section-team"><div><?php echo $lang['menu']['team']; ?></div></a></li>
+							<li><a href="#" data-href="#section-services"><div><?php echo $lang['menu']['services']; ?></div></a></li>
+							<li><a href="#" data-href="#section-contact"><div><?php echo $lang['menu']['contact']; ?></div></a></li>
+						</ul>
+
+					</nav><!-- #primary-menu end -->
+
+				</div>
+
+			</div>
+
+		</header><!-- #header end -->
+
+		<div class="clear"></div>
+
+		<!-- Content
+		============================================= -->
+		<section id="content">
+
+			<div class="content-wrap">
+
+				<section id="section-about" class="page-section">
+
+					<div class="container clearfix">
+
+						<div class="heading-block center">
+							<h2><?php echo $lang['menu']['about']; ?></h2>
+							<span><?php echo $lang['about']['sub-title']; ?></span>
+						</div>
+
+						<?php foreach ($lang['about']['cards'] as $key => $card): ?>
+
+						<div class="col_one_third nobottommargin <?php echo ($key == 2) ? 'col_last' : '' ?>">
+							<div class="feature-box media-box">
+								<div class="fbox-media">
+									<img src="images/services/<?php echo $key+1 ?>.jpg" alt="<?php echo $cards['title']; ?>">
+								</div>
+								<div class="fbox-desc">
+									<h3><?php echo $card['title']; ?></h3>
+									<p><?php echo $card['content']; ?></p>
+								</div>
+							</div>
+						</div>
+
+						<?php endforeach ?>
+
+						<div class="clear"></div>
+
+					</div>
+
+					<div class="section dark parallax nobottommargin" style="padding: 80px 0;background-image: url('images/parallax/1.jpg');" data-bottom-top="background-position:0px 100px;" data-top-bottom="background-position:0px -300px;">
+
+						<div class="container clearfix">
+
+							<div class="col_full nobottommargin center" data-animate="bounceIn">
+								<!--<i class="i-plain i-xlarge divcenter nobottommargin icon-code"></i>-->
+								<div class="counter counter-lined"><span data-from="0" data-to="100" data-refresh-interval="5" data-speed="2000"></span>+</div>
+								<h5>Años de experiencia combinada</h5>
+							</div>
+
+
+						</div>
+
+					</div>
+
+				</section>
+
+				<section id="section-work" class="page-section topmargin-lg">
+
+					<div class="heading-block center bottommargin-lg">
+						<h2><?php echo $lang['opportunities']['title']; ?></h2>
+						<span><?php echo $lang['opportunities']['subtitle-1']; ?></span>
+					</div>
+
+					<div class="container clearfix">	
+						<?php foreach ($lang['opportunities']['cards1'] as $key => $card): ?>
+							<div class="col_one_fourth <?php echo ($key == 3) ? 'col_last' : '' ?>">
+								<div class="feature-box fbox-center fbox-effect nobottomborder" data-animate="fadeIn">
+									<h3><?php echo $card['title'] ?></h3>
+									<p><?php echo $card['content'] ?></p>
+								</div>
+							</div>
+						<?php endforeach ?>
+					</div>					
+					
+					<div class="heading-block center bottommargin-lg">
+						<h2></h2>
+						<span><?php echo $lang['opportunities']['subtitle-2']; ?></span>
+					</div>
+
+					<div class="container clearfix">
+						<?php foreach ($lang['opportunities']['cards2'] as $key => $card): ?>
+							<div class="col_one_fourth <?php echo ($key == 3) ? 'col_last' : '' ?>">
+								<div class="feature-box fbox-center fbox-effect nobottomborder" data-animate="fadeIn">
+									<h3><?php echo $card['title'] ?></h3>
+									<p><?php echo $card['content'] ?></p>
+								</div>
+							</div>
+						<?php endforeach ?>
+					</div>
+
+					<div class="divider divider-short divider-center topmargin-lg"><i class="icon-star3"></i></div>
+	
+				</section>
+
+				<section id="section-team" class="page-section topmargin-lg">
+
+					<div class="heading-block center">
+						<h2><?php echo $lang['team']['title']; ?></h2>
+						<span><?php echo $lang['team']['subtitle']; ?></span>
+					</div>
+
+					<div class="container clearfix">
+						
+						<div class="row">
+							<?php foreach ($lang['team']['cards'] as $key => $card): ?>
+							<div class="col-lg-6 bottommargin">
+								<div class="team team-list clearfix">
+									<div class="team-image">
+										<img src="images/team/3.jpg" alt="<?php echo $card['name']; ?>">
+									</div>
+									<div class="team-desc">
+										<div class="team-title"><h4><?php echo $card['name']; ?></h4></div>
+										<ul class="team-content">
+											<?php foreach ($card['content'] as $copy) :?>
+												<li>
+													<?php echo $copy; ?>
+												</li>
+											<?php endforeach; ?>
+										</ul>
+									</div>
+								</div>
+							</div>
+							<?php endforeach ?>
+						</div>
+
+						<div class="clear"></div>
+					</div>
+
+					<div class="section parallax" style="background-image: url('images/parallax/3.jpg'); padding: 100px 0;" data-bottom-top="background-position:0px 100px;" data-top-bottom="background-position:0px -100px;">
+						<div class="heading-block center nobottomborder nobottommargin">
+							<h2><?php echo $lang['parallax']['copy']; ?></h2>
+						</div>
+					</div>
+
+				</section>
+
+				<section id="section-services" class="page-section topmargin-lg">
+
+					<div class="heading-block center bottommargin-lg">
+						<h2><?php echo $lang['services']['title']; ?></h2>
+						<span><?php echo $lang['services']['subtitle']; ?></span>
+					</div>
+
+					<div class="container clearfix">
+
+					<div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
+						<div class="carousel-inner">
+							<?php foreach ($lang['services']['sections'] as $key => $section): ?>
+								<div class="carousel-item <?php echo ($key == 0) ? 'active' : '' ?>">
+									<h3><?php echo $section['title'] ?></h3>
+									<ul>
+										<?php foreach ($section['content'] as $bullet): ?>
+											<li><?php echo $bullet ?></li>
+										<?php endforeach ?>
+									</ul>
+								</div>
+							<?php endforeach ?>
+						</div>
+					</div>
+
+					<div class="divider divider-short divider-center topmargin-lg"><i class="icon-star3"></i></div>
+
+				</section>
+
+				<section id="section-contact" class="page-section">
+
+					<div class="heading-block title-center">
+						<h2><?php echo $lang['contact']['title']; ?></h2>
+					</div>
+
+					<div class="container clearfix">
+
+						<!-- Contact Form
+						============================================= -->
+						<div class="col_half">
+
+							<div class="fancy-title title-dotted-border">
+								<h3><?php echo $lang['contact']['subtitle']; ?></h3>
+							</div>
+
+							<div class="contact-widget">
+
+								<div class="contact-form-result"></div>
+
+								<form class="nobottommargin" id="template-contactform" name="template-contactform" action="include/sendemail.php" method="post">
+
+								<div class="form-process"></div>
+
+								<div class="col_one_third">
+									<label for="template-contactform-name"><?php echo $lang['contact']['fields']['name']; ?> <small>*</small></label>
+									<input type="text" id="template-contactform-name" name="template-contactform-name" value="" class="sm-form-control required" />
+								</div>
+
+								<div class="col_one_third">
+									<label for="template-contactform-email"><?php echo $lang['contact']['fields']['email']; ?> <small>*</small></label>
+									<input type="email" id="template-contactform-email" name="template-contactform-email" value="" class="required email sm-form-control" />
+								</div>
+
+								<div class="col_one_third col_last">
+									<label for="template-contactform-phone"><?php echo $lang['contact']['fields']['phone']; ?></label>
+									<input type="text" id="template-contactform-phone" name="template-contactform-phone" value="" class="sm-form-control" />
+								</div>
+
+								<div class="clear"></div>
+
+								<div class="col_full">
+									<label for="template-contactform-subject"><?php echo $lang['contact']['fields']['subject']; ?> <small>*</small></label>
+									<input type="text" id="template-contactform-subject" name="template-contactform-subject" value="" class="required sm-form-control" />
+								</div>
+
+								<div class="clear"></div>
+
+								<div class="col_full">
+									<label for="template-contactform-message"><?php echo $lang['contact']['fields']['message']; ?> <small>*</small></label>
+									<textarea class="required sm-form-control" id="template-contactform-message" name="template-contactform-message" rows="6" cols="30"></textarea>
+								</div>
+
+								<div class="col_full hidden">
+									<input type="text" id="template-contactform-botcheck" name="template-contactform-botcheck" value="" class="sm-form-control" />
+								</div>
+
+								<div class="col_full">
+									<button class="button button-3d nomargin" type="submit" id="template-contactform-submit" name="template-contactform-submit" value="submit"><?php echo $lang['contact']['fields']['button']; ?></button>
+								</div>
+
+							</form>
+
+						</div>
+
+
+						</div><!-- Contact Form End -->
+
+						<!-- Google Map
+						============================================= -->
+						<div class="col_half col_last">
+
+							<section id="google-map" class="gmap" style="height: 410px;"></section>
+
+
+						</div><!-- Google Map End -->
+					</div>
+				</section>
+			</div>
+
+		</section><!-- #content end -->
+
+		<!-- Footer
+		============================================= -->
+		<footer id="footer" class="dark">
+
+			<!-- Copyrights
+			============================================= -->
+			<div id="copyrights">
+
+				<div class="container clearfix">
+
+					<div class="col_half">
+						2018 Copyrights &copy; Tasc Energía<br>
+						<div class="copyright-links"><a href="#">Aviso de Privacidad</a></div>
+					</div>
+
+					<div class="col_half col_last tright">
+						<div class="fright clearfix">
+						</div>
+
+						<div class="clear"></div>
+
+						<i class="icon-envelope2"></i> info@tasc-energia.com <span class="middot">&middot;</span> <i class="icon-headphones"></i> +91-11-6541-6369
+					</div>
+
+				</div>
+
+			</div><!-- #copyrights end -->
+
+		</footer><!-- #footer end -->
+
+	</div><!-- #wrapper end -->
+
+	<!-- Go To Top
+	============================================= -->
+	<div id="gotoTop" class="icon-angle-up"></div>
+
+	<!-- External JavaScripts
+	============================================= -->
+	<script src="js/jquery.js"></script>
+	<script src="js/plugins.js"></script>
+
+	<!-- Footer Scripts
+	============================================= -->
+	<script src="js/functions.js"></script>
+
+	<script src="https://maps.google.com/maps/api/js?key=AIzaSyDMxJ92oBkSnVNHFX3R8XhtYQPEgk1_IiI"></script>
+	<script src="js/jquery.gmap.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+	<script>
+		$('.carousel').carousel()
+
+		jQuery('#google-map').gMap({
+
+			address: 'Bosque de los Ciruelos 140, Bosque de las Lomas, Ciudad de México, CDMX',
+			maptype: 'ROADMAP',
+			zoom: 14,
+			markers: [
+				{
+					address: "Bosque de los Ciruelos, Bosque de las Lomas, Ciudad de México, CDMX",
+					html: '<div style="width: 300px;"><h4 style="margin-bottom: 8px;"><span>Tasc Energía</span></h4><p class="nobottommargin">Bosque de los Ciruelos 140-503, Bosque de las Lomas, Ciudad de México, CDMX</p></div>',
+					icon: {
+						image: "images/icons/map-icon-red.png",
+						iconsize: [32, 39],
+						iconanchor: [32,39]
+					}
+				}
+			],
+			doubleclickzoom: false,
+			controls: {
+				panControl: true,
+				zoomControl: true,
+				mapTypeControl: true,
+				scaleControl: false,
+				streetViewControl: false,
+				overviewMapControl: false
+			}
+
+		});
+	</script>
+
+</body>
+</html>
---
+++ languages/es.php
@@ -0,0 +1,197 @@
+<?php 
+//ESPANOL
+$lang = array(
+    'main' => [
+        'company' => 'Tasc Energía',
+        'description'=> 'Somos un equipo Directivo y Gerencial altamente calificado con más de 100 años de experiencia combinada en la Industria Mexicana de Aceite y Gas.'
+    ],
+    'menu' => [
+        'about' => 'Antecedentes',
+        'work' => 'Oportunidades',
+        'team' => 'Equipo',
+        'services' => 'Servicios',
+        'contact' => 'Contacto'
+    ],
+    'about' => [
+        'subtitle' => 'Somos un equipo Directivo y Gerencial altamente calificado con más de 100 años de experiencia combinada en la Industria Mexicana de Aceite y Gas',
+        'cards' => [
+            [
+                'title' => '¿Qué áreas manejamos?',
+                'content' => 'Nos orientamos a servicios de desarrollo de campos Petroleros tanto en tierra como Costa Afuera incluyendo Agua Profundas así como administración de proyectos del sector Aceite y gas.'
+            ],
+            [
+                'title' => 'Nuestro objetivo',
+                'content' => 'Nuestro principal objetivo es posicionarnos como una de las principales proveedoras de servicios y consultorías en el sector de Energía, generando oportunidades que ofrece la Reforma Energética en México, aprovechado las capacidades y experiencia de nuevos participantes. '
+            ],
+            [
+                'title' => '¿Qué hacemos?',
+                'content' => 'Ofrecemos servicios, asesoría y apoyo técnico a las Empresas Operadoras que buscan participación en el mercado de desarrollo de campos en México, aprovechando la apertura del Sector favorecida por la la Reforma Energética.'
+            ],
+
+        ]
+    ],
+    'opportunities' => [
+        'title' => 'Oportunidades del sector en la explotación de campos petroleros',
+        'subtitle-1' => 'La cantidad de oportunidades en el Sector Mexicano para el Desarrollo de Campos Petroleros es enorme e indiscutible', 
+        'cards1' => [
+            [
+                'title' => 'Reservas no confirmadas',
+                'content' => 'El 70% de las reservas totales en territorio mexicano, de aceite y Gas aún por confirmarse.'
+            ],
+            [
+                'title' => 'Capacidad local insuficiente',
+                'content' => 'La capacidad local actual de recursos en Exploración y Producción es insuficiente para satisfacer la creciente demanda.'
+            ],
+            [
+                'title' => 'Bloques',
+                'content' => 'Aún hay bloques por asignarse.'
+            ],
+            [
+                'title' => 'Reservas no desarrolladas',
+                'content' => 'Las reservas de hidrocarburos están parcialmente desarrolladas por lo que se requieren inversiones en perforación de pozos, nuevas instalaciones de proceso de hidrocarburos y ductos de transporte.'
+            ]
+        ],
+        'subtitle-2' => 'La compañías o nuevos operadores requieren apoyo de servicios especializados de consultoría.', 
+        'cards2' => [
+            [
+                'title' => 'El sector',
+                'content' => 'Tenemos un gran conocimiento del sector energía en México'
+            ],
+            [
+                'title' => 'Las entidades',
+                'content' => 'Conocemos ampliamente a las entidades gubernamentales reguladora que están involucradas.'
+            ],
+            [
+                'title' => 'Regulaciones',
+                'content' => 'Contamos con gran conocimiento de las regulaciones del sector.'
+            ],
+            [
+                'title' => 'Experiencia',
+                'content' => 'Nuestra experiencia en el desarrollo de los campos petroleros es enorme.'
+            ]
+        ],
+    ],
+    'team' => [
+        'title' => 'Nuestro equipo de trabajo',
+        'cards' => [
+            [
+                'name' => 'Antonio Acuña',
+                'content' => [
+                    'Más de 50 años con experiencia operativa en la industria del petróleo y gas en México.',
+                    'Presidente Ejecutivo (CEO) para C. P. Latina.',
+                    'Director Operativo de Oro Negro.',
+                    'Director de la división Costa Afuera (Offshore) en PEMEX por 40 años.',
+                    'Ingeniero Petrolero por la Universidad Autónoma de México con maestría de la Universidad de Manchester.'
+                ]
+            ],
+            [
+                'name' => 'Antonio García Arana',
+                'content' => [
+                    '35+ años de experiencia en Administración de Proyectos en la industria Mexicana de Aceite y gas.',
+                    'Gerente de Administración  de Compras de CP Latina',
+                    'Gerente de Desarrollo de Campos de CP. Latina',
+                    'Directivo en el Secretaria de Programación y Presupuesto del Gobierno Mexicano',
+                    'Ingeniero Civil por la UNAM con Maestrías en Planeación y en Investigacion de Operaciones'
+                ]
+            ],
+            [
+                'name' => 'José A. Ballesteros',
+                'content' => [
+                    'Socio Director de Ballesteros Bracho y Compañía, S. C.'.
+                    'Responsable de la división de consultoría, auditoría y proyectos especiales.',
+                    'Asesor Ejecutivo para la presidencia de empresas y sus directores generales. ',
+                    'Participación directa y personal en la adquisición de equipo de plataformas petroleras y experiencia en la relación contratistas-PEMEX por 8 años.',
+                    'Contador Público por la Universidad Autónoma de México.'
+                ]            
+            ],
+            [
+                'name' => 'Julian Rincón Acebo',
+                'content' => [
+                    'Más de 40 años en experiencia operativa en perforación en Tierra y Costa Afuera.',
+                    'Gerente de Operación para:
+                    -Petroleos Mexicanos: Supervisor de 30 equipos de perforación en Tierra y Semisumergibles, Jack Ups, Equipos Modulares.
+                    -Nabors: Supervisor de 5 equipos Modulares.
+                    -Pride: Supervisor de 2 Equipos Semisumergibles y 17 Jack Ups.
+                    -COSL: Supervisor de 2 Equipos Modulares.
+                    -Perforadora Latina. Gerente de Perforación 2 Jack Ups y un Equipo Modular.
+                    -Oro Negro: Gerente de Perforación 5 Jack Ups.',
+                    'Gerente de Desarrollo de campos para Petróleos Mexicanos: Cantarell, Abkatun, Ku, Maloob & Zaap.'
+                ]
+            ],
+            [
+                'name' => 'Fernando Blanco',
+                'content' => [
+                    'Más de 35 años con experiencia operativa  en la industria Mexicana del Petróleo y Gas.',
+                    'Responsable de las operaciones Costa Afuera de C. P. Latina.',
+                    'Representante técnico y Gerente de Operaciones Senior para COSL, Goimar y Protexa.',
+                    'Ingeniero Industrial por el Instituto Tecnológico Regional de Torreón, Coahuila,  México.'
+                ]
+            ],
+            [
+                'name' => 'Manuel Ferrer',
+                'content' => [
+                    'Más de 25 años con experiencia operativa en la industria Mexicana del Petróleo y Gas.',
+                    'Fungió como Gerente de Proyecto para la construcción y operación de los Equipos Modulares de CP Latina.',
+                    'Fue también Gerente de Operaciones para Diamond Offshore, Rowan Companies, Ensco Drilling, Nabors, y Noble.',
+                    'Maestría en Administración de Negocios, egresado de La Universidad Autónoma de Guadalajara.'
+                ]
+            ],
+            [
+                'name' => 'Luis Guillermo Ucha González',
+                'content' => [
+                    'Más de 35 años de experiencia operativa en la industria Mexicana del Petróleo y Gas.',
+                    '15+ años de experiencia en perforación, terminación e intervención a pozos.',
+                    '10+ años de experiencia en Desarrollo de campos en Aguas Profundas, sistemas submarinos y equipos flotantes de producción y almacenamiento.',
+                    '10+ años de experiencia en Administración de proyectos para la explotación de campos marinos.',
+                    'Ingeniero Petrolero egresado de la UNAM, con maestría en Administración de Negocios para ejecutivos en programa conjunto de Tulane University, Universidad Autónoma del Carmen y el ITESM Campus Monterrey y egresado del programa Dirección de Empresa D1 del IPADE.'
+                ]
+            ]
+        ]
+    ],
+    'parallax' => [
+        'copy' => 'Más de 100 años de experiencia combinada en la Industria Mexicana de Aceite y Gas nos respaldan'
+    ],
+    'services' => [
+        'title' => 'Servicios',
+        'subtitle' => '',
+        'sections' => [
+            [
+                'title' => 'Servicios y consultoría en:',
+                'content' => [
+                    'Evaluación de prospectos exploratorios.',
+                    'Análisis de información en Cuarto de Datos (Data Room).',
+                    'Preparación de planes de desarrollo.',
+                    'Preparación de ofertas para licitación y compra de equipos.',
+                    'Participación en Concursos.',
+                    'Negociación de contratos.',
+                    'Administración de contratos',
+                    'Planeación del Mantenimiento a plataformas marinas.',
+                    'Vinculación con las empresas gubernamentales del sector de Energía.'
+                ],
+            ],
+            [
+                'title' => 'Operación',
+                'content' => [
+                    'Operación de Campos.',
+                    'Asesoría en administración de proyectos.',
+                    'Encargado de la operación.',
+                    'Resolución de problemas.',
+                    'Operación y Mantenimiento de Plataformas Autoelevables.'
+                ]
+            ]
+        ]
+    ],
+    'contact'=> [
+        'title' => 'Contacto',
+        'subtitle' => 'Envíanos un correo',
+        'fields' => [
+            'name' => 'Nombre',
+            'email' => 'Correo',
+            'phone' => 'Teléfono',
+            'subject' => 'Asunto',
+            'message' => 'Mensaje',
+            'button' => 'Enviar mensaje'
+        ]
+     ]
+
+);
